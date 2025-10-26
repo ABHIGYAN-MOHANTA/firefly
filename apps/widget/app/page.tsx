0048962a736d429681d3f3c7ec4a20aef3204024
abhigyan-mohanta <underthunder02@gmail.com>
2025-10-26 11:50:40 +0530

    feat add convex realtime db

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,19 @@
-import { add } from "@workspace/math/add"
+"use client"
+
+import { useQuery } from "convex/react"
 import { Button } from "@workspace/ui/components/button"
+import { Input } from "@workspace/ui/components/input"
+import { add } from "@workspace/math/add"
+import { api } from "@workspace/backend/_generated/api"
+
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
-        <Button size="sm">Button</Button>
-        <p>Result: {add(2, 2)}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/widget</p>
+      <div className="max-w-sm w-full mx-auto"></div>
+      <p>{JSON.stringify(users, null, 2)}</p>
     </div>
   )
 }
